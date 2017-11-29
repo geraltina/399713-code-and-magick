c976bf8134cd51ced789bfa75633e32995bf3f3b
geraltina <evgpestr@yandex.ru>
2017-11-29 12:01:18 +0300

    открытие/закрытие popup, валидация формы и изменение внешнего вида волшебника

--- js/setup.js
+++ js/setup.js
@@ -1,5 +1,8 @@
 'use strict';
 
+var ESC_KEYCODE = 27;
+var ENTER_KEYCODE = 13;
+
 var NAMES = [
   'Иван',
   'Хуан Себастьян',
@@ -39,21 +42,104 @@ var EYESCOLORS = [
   'green'
 ];
 
+var FIREBALLCOLORS = [
+  '#ee4830',
+  '#30a8ee',
+  '#5ce6c0',
+  '#e848d5',
+  '#e6e848'
+];
+
 var setup = document.querySelector('.setup');
 var setupOpen = document.querySelector('.setup-open');
 var setupClose = document.querySelector('.setup-close');
+var setupSubmit = document.querySelector('.setup-submit');
 var setupSimilar = document.querySelector('.setup-similar');
 var setupListElement = document.querySelector('.setup-similar-list');
 var setupWizardTemplate = document.querySelector('#similar-wizard-template').content.querySelector('.setup-similar-item');
 
-setupOpen.addEventListener('click', function () {
+// Checking validity of form
+var userNameInput = setup.querySelector('.setup-user-name');
+
+userNameInput.addEventListener('invalid', function () {
+  if (userNameInput.validity.tooShort) {
+    userNameInput.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else if (userNameInput.validity.tooLong) {
+    userNameInput.setCustomValidity('Имя не должно превышать 25-ти символов');
+  } else if (userNameInput.validity.valueMissing) {
+    userNameInput.setCustomValidity('Обязательное поле');
+  } else {
+    userNameInput.setCustomValidity('');
+  }
+});
+
+userNameInput.addEventListener('input', function (evt) {
+  var target = evt.target;
+  if (target.value.length < 2) {
+    target.setCustomValidity('Имя должно состоять минимум из 2-х символов');
+  } else {
+    target.setCustomValidity('');
+  }
+});
+
+// Showing and closing popup
+var onPopupEscPress = function (evt) {
+  if (evt.keyCode === ESC_KEYCODE) {
+    closePopup();
+  }
+};
+
+var openPopup = function () {
   setup.classList.remove('hidden');
+
+  document.addEventListener('keydown', function (evt) {
+    if (evt.keyCode === ESC_KEYCODE) {
+      setup.classList.add('hidden');
+    }
+  });
+
+  // If input with name is valid, closes popup, else - doesn't
+  if (userNameInput.validity === true) {
+    setupSubmit.addEventListener('click', function () {
+      closePopup();
+    });
+
+    setupSubmit.addEventListener('keydown', function (evt) {
+      if (evt.keyCode === ENTER_KEYCODE) {
+        closePopup();
+      }
+    });
+  } else {
+    setup.classList.remove('hidden');
+  }
+};
+
+var closePopup = function () {
+  setup.classList.add('hidden');
+  document.removeEventListener('keydown', onPopupEscPress);
+};
+
+setupOpen.addEventListener('click', function () {
+  openPopup();
+});
+
+setupOpen.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    openPopup();
+  }
 });
 
 setupClose.addEventListener('click', function () {
-  setup.classList.add('hidden');
+  closePopup();
 });
 
+setupClose.addEventListener('keydown', function (evt) {
+  if (evt.keyCode === ENTER_KEYCODE) {
+    closePopup();
+  }
+});
+
+// Getting random values
 var getRandomArrayIndex = function (array) {
   return Math.floor(Math.random() * array.length);
 };
@@ -66,6 +152,7 @@ var getNameAndSurname = function (arrayOne, arrayTwo) {
   return arrayOne[getRandomArrayIndex(arrayOne)] + ' ' + arrayTwo[getRandomArrayIndex(arrayTwo)];
 };
 
+// Creating similar wizards
 var wizards = [];
 
 for (var j = 0; j < 4; j++) {
@@ -93,3 +180,20 @@ for (var i = 0; i < wizards.length; i++) {
 
 setupListElement.appendChild(fragment);
 setupSimilar.classList.remove('hidden');
+
+// Changing wizard appearance
+var wizardCoat = document.querySelector('.setup-wizard .wizard-coat');
+var wizardEyes = document.querySelector('.setup-wizard .wizard-eyes');
+var wizardFireball = document.querySelector('.setup-fireball-wrap');
+
+wizardCoat.addEventListener('click', function () {
+  wizardCoat.style.fill = getRandomArrayWord(COATCOLORS);
+});
+
+wizardEyes.addEventListener('click', function () {
+  wizardEyes.style.fill = getRandomArrayWord(EYESCOLORS);
+});
+
+wizardFireball.addEventListener('click', function () {
+  wizardFireball.style.backgroundColor = getRandomArrayWord(FIREBALLCOLORS);
+});
